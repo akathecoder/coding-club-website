1c24d5f54e0d35a0d148e66adb21c809d2378716
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-09-14 23:02:19 +0530

    styled the scroll bar

--- components/Sections/Our-Team/our-team.tsx
+++ components/Sections/Our-Team/our-team.tsx
@@ -8,7 +8,7 @@ function OurTeam() {
     if (error) return <></>;
 
     return (
-        <div className="bg-background h-screen text-center py-3">
+        <div className="bg-background h-screen text-center py-3 border-t border-accent6">
             <h1 className="text-6xl my-8">Our Team</h1>
             <div className="grid grid-cols-4 gap-8 mt-20">
                 {data && data.members.map((member, idx) => <TeamMember {...member} key={idx} />)}
