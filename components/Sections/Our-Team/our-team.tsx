e115939e9070c7f68dc1bb512c7f2d9cf1b8664b
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-09-20 22:04:39 +0530

    added padding-x on team members

--- components/Sections/Our-Team/our-team.tsx
+++ components/Sections/Our-Team/our-team.tsx
@@ -8,9 +8,9 @@ function OurTeam() {
     if (error) return <></>;
 
     return (
-        <div className="bg-background h-screen text-center py-3 border-t border-accent6">
-            <h1 className="text-6xl my-8">Our Team</h1>
-            <div className="grid grid-cols-4 gap-8 mt-20">
+        <div className="bg-background text-center pb-32 border-t border-accent6">
+            <h1 className="text-6xl my-10">Our Team</h1>
+            <div className="grid grid-cols-4 gap-14 mt-20 max-w-7xl mx-auto">
                 {data && data.members.map((member, idx) => <TeamMember {...member} key={idx} />)}
             </div>
         </div>
