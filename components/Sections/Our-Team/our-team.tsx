589e86a45b958558bfc616beb18dadec09dc3d9a
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-09-23 19:04:05 +0530

    our-team section responsiveness improved

--- components/Sections/Our-Team/our-team.tsx
+++ components/Sections/Our-Team/our-team.tsx
@@ -4,7 +4,7 @@ import TeamMember from '../../General/team-member/TeamMember';
 
 function OurTeam({ members }: IOurTeam) {
     return (
-        <div className="bg-background text-center px-20 py-3 pb-20 border-t border-accent6">
+        <div className="bg-background text-center px-10 py-3 pb-20 border-t border-accent6 sm:px-20">
             <h1 className="text-6xl my-8">Our Team</h1>
             <div className="grid grid-cols-2 gap-8 mt-20 max-w-7xl m-auto sm:grid-cols-3 lg:grid-cols-4">
                 {members.map((member, idx) => (
