58cf29d29908e1f69287fc70229afc9fc8330f5f
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-09-23 17:59:16 +0530

    made the events page responsive

--- pages/events.tsx
+++ pages/events.tsx
@@ -8,7 +8,7 @@ function Events({ data }: EventsResponse) {
             <header className="py-10">
                 <h1 className="text-6xl">Events</h1>
             </header>
-            <div className="grid grid-cols-2 gap-20 mt-5">
+            <div className="grid grid-cols-1 gap-20 mt-5 px-10 s-800:grid-cols-2">
                 {data.events.map((event, idx) => (
                     <Event key={idx} {...event} />
                 ))}
