1a2a22013877c1d2a6e9da0bb3c4cc6a4885fd11
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-09-27 02:02:15 +0530

    added animations to navbar

--- components/General/navbar/Navbar.tsx
+++ components/General/navbar/Navbar.tsx
@@ -7,24 +7,32 @@ import Link from 'next/link';
 
 interface NavbarProps {}
 
-const NavLinksList: React.FC<{ smallerThan600px: boolean }> = ({ smallerThan600px }) => {
+const NavLinksList: React.FC<{ smallerThan600px?: boolean; onClick?: () => void }> = ({
+    smallerThan600px = false,
+    onClick,
+}) => {
     const links = [
         { name: 'About', link: '!#' },
         { name: 'Events', link: '/events' },
         { name: 'Gallery', link: '!#' },
         { name: 'Our Team', link: '!#' },
         { name: 'Resources', link: '!#' },
+        { name: 'Projects', link: '/projects' },
         { name: 'Others', link: '!#' },
     ];
 
     return (
-        <ul className={`flex ${smallerThan600px ? 'flex-col items-center' : ''}`}>
-            {links.map(({ name, link }, idx) => (
-                <li key={idx} className={`mx-3 text-xl mb-2 relative`}>
-                    <Link href={link}>{name}</Link>
-                </li>
-            ))}
-        </ul>
+        <div className={`w-full ${smallerThan600px ? 'absolute py-5 bg-gray' : ''} ${styles.navLinks}`}>
+            <ul className={`flex ${smallerThan600px ? 'flex-col items-center' : 'justify-center'}`}>
+                {links.map(({ name, link }, idx) => (
+                    <button key={idx} onClick={onClick}>
+                        <li className={`mx-3 text-xl mb-1 relative`}>
+                            <Link href={link}>{name}</Link>
+                        </li>
+                    </button>
+                ))}
+            </ul>
+        </div>
     );
 };
 
@@ -32,11 +40,25 @@ const Navbar: React.FC<NavbarProps> = () => {
     const smallerThan600px = useMediaQuery('(max-width: 600px)');
     const [open, setOpen] = useState<boolean>(false);
 
+    function closeNavLinks() {
+        setOpen(false);
+    }
+
+    function toggleNavbar() {
+        setOpen((o) => !o);
+    }
+
     useEffect(() => {
         if (!smallerThan600px) {
             setOpen(false);
         }
-    }, [smallerThan600px]);
+
+        if (open) {
+            document.body.style.overflowY = 'hidden';
+        } else {
+            document.body.style.overflowY = 'scroll';
+        }
+    }, [smallerThan600px, open, setOpen]);
 
     return (
         <Fragment>
@@ -50,32 +72,23 @@ const Navbar: React.FC<NavbarProps> = () => {
             >
                 {smallerThan600px && (
                     <div className="px-4 cursor-pointer h-full flex justify-center items-center mx-3">
-                        <HamMenu onClick={() => setOpen((o) => !o)} open={open} />
+                        <HamMenu onClick={toggleNavbar} open={open} />
                     </div>
                 )}
-                <div
-                    className={`flex justify-center items-center w-full ${styles.navLinks} ${
-                        smallerThan600px ? 'absolute h-auto bg-gray top-12' : 'h-full '
-                    }`}
-                >
-                    {smallerThan600px && open ? (
-                        <NavLinksList smallerThan600px={smallerThan600px} />
-                    ) : (
-                        <Fragment>
-                            {!smallerThan600px ? (
-                                <NavLinksList smallerThan600px={smallerThan600px} />
-                            ) : (
-                                <Fragment></Fragment>
-                            )}
-                        </Fragment>
-                    )}
-                </div>
+
+                {smallerThan600px ? (
+                    <Fragment>
+                        {open ? <NavLinksList smallerThan600px onClick={closeNavLinks} /> : <Fragment></Fragment>}
+                    </Fragment>
+                ) : (
+                    <NavLinksList />
+                )}
 
                 <Portal selector="backdrop">
                     {open && smallerThan600px && (
                         <div
                             className="absolute w-full h-full bg-gray left-0 top-0 right-0 z-10 opacity-90"
-                            onClick={() => setOpen(false)}
+                            onClick={closeNavLinks}
                         ></div>
                     )}
                 </Portal>
